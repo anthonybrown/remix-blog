31645c6939ebe1a988982fb90b5504f2c116725c
Tony Brown <tony@tonybrown.me>
2022-10-10 11:01:19 -0400

    add all files to commit

---
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -0,0 +1,37 @@
+import { Link, useLoaderData } from '@remix-run/react'
+import { json } from '@remix-run/node'
+import { prisma } from '~/db.server'
+
+async function getPosts() {
+  return prisma.post.findMany()
+}
+
+export async function loader() {
+  const posts = await getPosts()
+
+  return json({ posts })
+  // return json(posts)
+  // return new Response(JSON.stringify(posts), {
+  //   headers: {
+  //     'Content-type': 'application/json',
+  //   },
+  // })
+}
+
+export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>()
+  return (
+    <main className="container mx-auto">
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
